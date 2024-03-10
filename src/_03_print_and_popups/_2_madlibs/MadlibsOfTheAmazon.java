7980af15f01b084273e365320ed2379c1c4ab268
Gryffin2021 <gryffinwilson@gmail.com>
2024-03-09 17:29:20 -0800

    Quiz Game

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -1,4 +1,4 @@
-package _03_print_and_popups._2_madlibs;
+ package _03_print_and_popups._2_madlibs;
 
 import javax.swing.JOptionPane;
 
@@ -17,26 +17,22 @@ public class MadlibsOfTheAmazon {
 		// Get the player to enter a verb
 		String input3 = JOptionPane.showInputDialog("write a verb");
 		// Get the player to enter a place
-		String input4 = JOptionPane.showInputDialog("write yhe name of a place?");
+		String input4 = JOptionPane.showInputDialog("write the name of a place?");
 		// The story below has has been written as a group of Strings joined together by + signs.
 		// The story contains place holders, indicated by [** **] which you need to replace with
 		// the values entered by the player.
 		// Hint:  You will need to add more + signs to join the variables to the other parts of the story.
 		
 		String story = 
-		"Piranhas are more **input** during the day, so cross the river at\n"  +
-		"night. Piranhas are attracted to fresh [**input2**] and will most\n"  +
-		"likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"  +
+		"Piranhas are more "+input+" during the day, so cross the river at\n"  +
+		"night. Piranhas are attracted to fresh "+input1+" and will most\n"  +
+		"likely take a bite out of your "+input2+" if you "+input3+". Whatever\n"  +
 		"you do, if you have an open wound, try to find another way to get\n"  +
-		"back to the [**place**]. Good luck!";
+		"back to the "+input4+". Good luck!";
 		
 		// Make a pop-up that contains the final story. The \n escape characters add line breaks to the story. 
 		// If you need to, move them around to make your story look better in the pop-up
-		JOptionPane.showMessageDialog(null, "Piranhas are more" +input "during the day, so cross the river at\n"  +
-				"night. Piranhas are attracted to fresh" +input1 "and will most\n"  +
-				"likely take a bite out of your +input2 if you +input3. Whatever\n"  +
-				"you do, if you have an open wound, try to find another way to get\n"  +
-				"back to the" +input4 "Good luck!");
+		JOptionPane.showMessageDialog(null,story);
 		
 		// If you want to write your own Madlib story, just change the story variable and ask the player different questions.
 
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -4,13 +4,16 @@ public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+		String input10 = JOptionPane.showInputDialog("What is the Yo form of The spanish verb, estar?");
 				// 3.  Use an if statement to check if their answer is correct
+				if (input10.equals("estoy");
 				
+				/ else
+			
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
